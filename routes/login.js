0e0f167bcd5395f38e1c63ff1744d0e2fed41a72
Andres <andresbrusutti@gmail.com>
2018-01-16 07:53:08 -0300

    Sección 12 terminada - backend terminado

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,10 @@
 var express = require('express'); //Server Node Express
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
-var SEED = require('../config/config').SEED;
+
+const SEED = require('../config/config').SEED;
+const GOOGLE_CLIENT_ID = require('../config/config').GOOGLE_CLIENT_ID;
+const GOOGLE_SECRET_ID = require('../config/config').GOOGLE_SECRET_ID;
 
 // Inicializar variables
 var app = express();
@@ -12,6 +15,8 @@ var app = express();
 // Recuperar Modelo de Usuario
 var Usuario = require('../models/usuario');
 
+var GoogleAuth = require('google-auth-library');
+var auth = new GoogleAuth;
 
 // ========================================
 //  PROBAR QUE ANDA EL ROUTE LOGIN
@@ -24,7 +29,104 @@ app.get('/', ( req, res) => {
 });
 
 // ========================================
-//  LOGUEAR UN USUARIO
+//  LOGUEAR UN USUARIO GOOGLE
+// https://developers.google.com/identity/sign-in/web/backend-auth
+// ========================================
+app.post('/google',( req, res ) => {
+
+    var token = req.body.token || 'XXX';
+    var client = new auth.OAuth2(GOOGLE_CLIENT_ID, GOOGLE_SECRET_ID, '');
+
+    client.verifyIdToken(
+        token,
+        GOOGLE_CLIENT_ID,
+        function(err, login) {
+            if ( err )
+            {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al validar token google',
+                    errors: err
+                });
+            }
+
+            var payload = login.getPayload();
+            var userid = payload['sub'];
+
+            Usuario.findOne({email: payload.email}, ( err , usuario ) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        mensaje: 'Error al buscar usuarios',
+                        errors: err
+                    });
+                }
+
+                if( usuario )
+                {
+                    //Control si no fue un usuario creado desde google
+                    if( !usuario.google )
+                    {
+                        return res.status(400).json({
+                            ok: false,
+                            mensaje: 'Debe usar la autenticacion normal email-pasword',
+                            errors: err
+                        });
+                    }else{
+                        // Es un usuario comun de email y password le devuelvo un token
+
+                        // Crear TOKEN ( payload, sid , fecha_expiracion (14400 = 4hs))
+                        usuario.password = ':)'; //Elimino la password para no mandarla en el TOKEN
+                        var token = jwt.sign({ usuario:usuario }, SEED, { expiresIn:14400 });
+
+                        return res.status(200).json({
+                            ok: true,
+                            usuario: usuario,
+                            token: token,
+                            id: usuario._id
+                        });
+                    }
+                }else{
+                    // Si el usuario no existe para ese correo lo doy de alta
+                    var usuario = new Usuario();
+                    usuario.nombre = payload.name;
+                    usuario.email = payload.email;
+                    usuario.password = ':)';
+                    usuario.img = payload.picture;
+                    usuario.google = true;
+
+                    usuario.save ( ( err, usuarioPersistido ) => {
+                        if (err) {
+                            return res.status(500).json({
+                                ok: false,
+                                mensaje: 'Error al guardar usuario',
+                                errors: err
+                            });
+                        }
+                    });
+                    // Crear TOKEN ( payload, sid , fecha_expiracion (14400 = 4hs))
+                    usuario.password = ':)'; //Elimino la password para no mandarla en el TOKEN
+                    var token = jwt.sign({ usuario:usuario }, SEED, { expiresIn:14400 });
+
+                    return res.status(200).json({
+                        ok: true,
+                        usuario: usuario,
+                        token: token,
+                        id: usuario._id
+                    });
+                }
+            });
+
+
+
+
+
+        });
+
+});
+
+// ========================================
+//  LOGUEAR UN USUARIO EMAIL-PASSWORD
 // ========================================
 
 app.post('/',( req, res ) => {
